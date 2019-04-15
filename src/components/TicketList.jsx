8128786cbc6c8bc262952fb0528f5c22848232cc
David Monarrez and Gary Bermudez <david@>
2019-04-15 11:33:12 -0700

    add components with props

---
+++ src/components/TicketList.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Ticket from "./Ticket";
+
+function TicketList(){
+  return (
+    <Ticket
+      location = "3A"
+      names = "Thato and Haley"
+      issue = "firebase will not save record"/>
+  );
+}
+
+export default TicketList;
