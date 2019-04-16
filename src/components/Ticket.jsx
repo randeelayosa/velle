810595d479bcbe9b43c37a4f8317e4fbfe26b777
Gary Bermudez <garybm.17@gmail.com>
2019-04-15 21:13:48 -0700

    add lint

--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,25 +1,25 @@
-import React from "react";
-import PropTypes from "prop-types";
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function Ticket(props){
   var myStyledComponentStyles = {
-   backgroundColor: '#ecf0f1',
-   fontFamily: 'sans-serif',
-   paddingTop: '50px'
- }
+    backgroundColor: '#ecf0f1',
+    fontFamily: 'sans-serif',
+    paddingTop: '50px'
+  }
   return (
-      <div style={myStyledComponentStyles}>
-        <h3> {props.location} - {props.names}</h3>
-        <p><em>{props.issue}</em></p>
-        <hr/>
-      </div>
-   );
+    <div style={myStyledComponentStyles}>
+      <h3> {props.location} - {props.names}</h3>
+      <p><em>{props.issue}</em></p>
+      <hr/>
+    </div>
+  )
 }
 
 Ticket.propTypes = {
   names: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
-};
+}
 
-export default Ticket;
+export default Ticket
