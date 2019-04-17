d9cf639cda8f19ec635616c1272c739e72bf470a
Student <example@example.com>
2019-04-17 11:05:48 -0700

    add links

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -4,8 +4,21 @@ import React from 'react';
 function NewTicketForm(){
   return(
     <div>
+      <form>
+        <input
+         type='text'
+         id='names'
+         placeholder='Pair Names'/>
+       <input
+         type='text'
+         id='location'
+         placeholder='Location'/>
+       <textarea
+         id='issue'
+         placeholder='Describe your issue'/>
+       <button type='submit'>Help!</button>
+      </form>
     </div>
-
   );
 }
 
