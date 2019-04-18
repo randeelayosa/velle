e70dd85ae089f6d89b6a5b83385675bc3ac03e73
Student <example@example.com>
2019-04-18 08:19:50 -0700

    add img loader

--- src/components/NewTicketForm.jsx
+++ src/components/NewTicketForm.jsx
@@ -1,22 +1,51 @@
 import React from 'react';
-
+import patos from '../assets/images/patos.jpeg';
 
 function NewTicketForm(){
+
+  var styles = {
+      backgroundImage: 'linearGradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5))'
+  };
   return(
-    <div>
+    <div style={styles}>
+      <div>
+        <style jsx>{`
+            img {
+              width: 100%;
+              height: 25%;
+              background-position: center;
+              background-repeat: no-repeat;
+              background-size: cover;
+              position: relative;
+            }
+                `}</style>
+        <img src={patos}/>
+      </div>
       <form>
+        <style jsx>{`
+          form {
+            text-align: center;
+            position: absolute;
+            top: 75%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+            color: white;
+            width: 100%;
+            height:75%;
+          }
+        `}</style>
         <input
-         type='text'
-         id='names'
-         placeholder='Pair Names'/>
-       <input
-         type='text'
-         id='location'
-         placeholder='Location'/>
-       <textarea
-         id='issue'
-         placeholder='Describe your issue'/>
-       <button type='submit'>Help!</button>
+          type='text'
+          id='names'
+          placeholder='Pair Names'/>
+        <input
+          type='text'
+          id='location'
+          placeholder='Location'/>
+        <textarea
+          id='issue'
+          placeholder='Describe your issue'/>
+        <button type='submit'>Help!</button>
       </form>
     </div>
   );
