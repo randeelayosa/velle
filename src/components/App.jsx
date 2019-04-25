da8b62245b436c2d1286e650a1a546d5727e3ede
Gary Bermudez and Victoria Martinez <victoria@>
2019-04-25 16:31:08 -0700

    add create reusable components and conditional functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,7 +4,8 @@ import TicketList from './TicketList'
 import NewTicketControl from './NewTicketControl'
 import Error404 from './Error404'
 import { Switch, Route } from 'react-router-dom'
-import Moment from 'moment';
+import Moment from 'moment'
+import Admin from './Admin'
 
 
 class App extends React.Component {
@@ -12,9 +13,11 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      masterTicketList: []
+      masterTicketList: [],
+      selectedTicket: null
     }
-    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this)
+    this.handleAddingNewTicketToList = this.handleAddingNewTicketToList.bind(this);
+    this.handleChangingSelectedTicket = this.handleAddingNewTicketToList.bind(this);
   }
 
   componentDidMount() {
@@ -43,6 +46,11 @@ class App extends React.Component {
     this.setState({masterTicketList: newMasterTicketList})
   }
 
+  handleChangingSelectedTicket(ticket){
+    this.setState({selectedTicket: ticket});
+    alert('The selected ticket is now: ' + this.state.selectedTicket.names);
+  }
+
   render(){
     return (
       <div>
@@ -50,6 +58,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' render={()=><TicketList ticketList={this.state.masterTicketList} />} />
           <Route path='/newticket' render={()=><NewTicketControl onNewTicketCreation={this.handleAddingNewTicketToList} />} />
+          <Route path='/admin' render={(props)=><Admin ticketList={this.state.masterTicketList} currentRouterPath={props.location.pathname}  onTicketSelection={this.handleChangingSelectedTicket} selectedTicket={this.state.selectedTicket}/>} />
           <Route component={Error404} />
         </Switch>
       </div>
--- src/components/Ticket.jsx
+++ src/components/Ticket.jsx
@@ -1,33 +1,30 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import Moment from 'moment';
+
 
 function Ticket(props){
+  const ticketInformation =
 
-  return (
     <div>
-      <style global jsx>{`
-      div {
-        background-color: pink;
-        text-align: center;
-      }
-      h3 {
-        color: white;
-      }
-      p:hover {
-        color: white;
-        cursor: pointer;
-      }
-    `}</style>
       <h3> {props.location} - {props.names}</h3>
       <h4>{props.formattedWaitTime}</h4>
-      <p><em>{props.issue}</em></p>c
+      <p><em>{props.issue}</em></p>
       <hr/>
     </div>
-  )
-}
-function displayTimeOpen(timeOpen) {
-  return timeOpen.from(new Moment(), true);
+
+  if( props.currentRouterPath === '/admin') {
+    return (
+      <div onClick={() => {props.onTicketSelection({names: props.names, location: props.location, issue: props.issue, formattedWaitTime: props.formattedWaitTime});}}>
+        {ticketInformation}
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        {ticketInformation}
+      </div>
+    )
+  }
 }
 
 Ticket.propTypes = {
@@ -35,6 +32,8 @@ Ticket.propTypes = {
   location: PropTypes.string.isRequired,
   issue: PropTypes.string,
   formattedWaitTime: PropTypes.string.isRequired,
-};
+  currentRouterPath: PropTypes.string,
+  onTicketSelection: PropTypes.func
+}
 
 export default Ticket
