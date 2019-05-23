5f40c4272cfc7fb50f3e5b9d5e11a9249cd1d06a
Student <example@example.com>
2019-05-22 18:23:17 -0700

    Add data to firestore. Add spread operator dependency and plugin.

--- src/actions/wishlistActions.js
+++ src/actions/wishlistActions.js
@@ -1,6 +1,20 @@
 export const createList = (wishlist) => {
   return (dispatch, getState, { getFirebase, getFirestore }) => {
     //make async call to database
-    dispatch({type: 'CREATE_WISHLIST', wishlist})
+    const firestore = getFirestore();
+    firestore.collection('wishlists').add({
+      ...wishlist,
+      person: 'Anonymous',
+      gender: 'female',
+      age: '31',
+      size: 'AM',
+      need: 'business casual blouses',
+      authorId: 12345,
+      createdAt: new Date()
+    }).then(() => {
+      dispatch({type: 'CREATE_WISHLIST', wishlist});
+    }).catch((error) => {
+      dispatch({ type: 'CREATE_WISHLIST_ERROR', error});
+    })
   }
 };
--- src/reducers/wishlistReducer.js
+++ src/reducers/wishlistReducer.js
@@ -4,7 +4,12 @@ import { initialListState } from '../constants/initialState';
 export const wishlistReducer = (state = initialListState, action) => {
   switch (action.type) {
     case 'CREATE_WISHLIST':
-      console.log('created project', action.wishlist)
+      console.log('created project', action.wishlist);
+      return state;
+    case 'CREATE_WISHLIST_ERROR':
+      console.log('create wishlist error', action.error);
+      return state;
+      default:
+        return state;
   }
-  return state;
 };
